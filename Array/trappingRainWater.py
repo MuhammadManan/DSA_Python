2e82bd734ef02fb7ccaff7d3fd40537b57c23296
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-25 23:46:09 +0500

    Remove redundant trapped water calculation logic from trap function

--- Array/trappingRainWater.py
+++ Array/trappingRainWater.py
@@ -4,17 +4,6 @@ def trap(height):
     left_max, right_max = 0, 0
     trapped_water = 0
 
-    while left < right:
-        if height[left] < height[right]:
-            left_max = max(left_max, height[left])
-            trapped_water += left_max - height[left]
-            left += 1
-        else:
-            right_max = max(right_max, height[right])
-            trapped_water += right_max - height[right]
-            right -= 1
-
-    return trapped_water
 
 # Example usage:
 if __name__ == "__main__":

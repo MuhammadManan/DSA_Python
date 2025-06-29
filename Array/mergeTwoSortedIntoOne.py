25d2396f4e68d2a27d35e8b86b8af6a993341c89
Muhammad Manan Azhar <muhammadmananazhar1212@gmail.com>
2025-06-29 23:54:06 +0500

    Reformat example usage for merge_two_sorted_arrays function

--- Array/mergeTwoSortedIntoOne.py
+++ Array/mergeTwoSortedIntoOne.py
@@ -22,6 +22,6 @@ def merge_two_sorted_arrays(arr1, arr2):
     return merged
 
 # Example usage:
-# arr1 = [1, 3, 5]
-# arr2 = [2, 4, 6]
-# print(merge_two_sorted_arrays(arr1, arr2))  # Output: [1, 2, 3, 4, 5, 6]
+arr1 = [1, 3, 5]
+arr2 = [2, 4, 6]
+print(merge_two_sorted_arrays(arr1, arr2))  # Output: [1, 2, 3, 4, 5, 6]
